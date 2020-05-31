4afd3774ced04c0307e772b80c276e95b2af9ac9
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-05-31 20:31:20 +0200

    Implemented command F Draw

--- kittyTurbo_files/ext_turbo.c
+++ kittyTurbo_files/ext_turbo.c
@@ -1043,10 +1043,63 @@ char *turboplusBlitIntOff KITTENS_CMD_ARGS
 	return tokenBuffer;
 }
 
+char *_turboplusFDraw( struct glueCommands *data, int nextToken )
+{
+	struct KittyInstance *instance = data -> instance;
+	struct retroScreen *screen = instance -> screens[instance -> current_screen];
+	int args =__stack - data->stack +1 ;
+	int x0,y0,x1=0,y1=0;
+
+	x0 = instance->xgr;
+	y0 = instance->ygr;
+
+	proc_names_printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
+
+	switch (args)
+	{
+		case 2:
+			instance->xgr = x1 = getStackNum(instance,__stack-1 );
+			instance->ygr = y1 = getStackNum(instance,__stack );
+
+			break;
+
+		case 4:
+			stack_get_if_int(instance,__stack-3, &x0 );
+			stack_get_if_int(instance,__stack-2, &y0 );
+			instance->xgr = x1 = getStackNum(instance,__stack-1 );
+			instance->ygr = y1 = getStackNum(instance,__stack );
+
+			if (screen) retroLine( screen, screen -> double_buffer_draw_frame,x0,y0,x1,y1,screen -> ink0 );
+			break;
+
+		default:
+			api.setError(22,data->tokenBuffer);
+	}
+
+	popStack(instance,__stack - data->stack );
+
+	switch (screen ->autoback)
+	{
+		case 0:
+				if (screen) retroLine( screen, screen -> double_buffer_draw_frame,x0,y0,x1,y1,screen -> ink0 );
+				break;
+
+		default:
+				if (screen)
+				{
+					retroLine( screen, 0,x0,y0,x1,y1,screen -> ink0 );
+					if (screen -> Memory[1])	 retroLine( screen, 1,x0,y0,x1,y1,screen -> ink0 );
+				}
+				break;
+	}
+
+	return NULL;
+}
+
 char *turboplusFDraw KITTENS_CMD_ARGS
 {
 	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
-	api.setError(22, tokenBuffer);
+	stackCmdNormal( _turboplusFDraw, tokenBuffer );
 	return tokenBuffer;
 }
 
