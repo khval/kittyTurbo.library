25c2e62ef4a96ff0107a0f23a92c8b8cf4a5dcb9
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-07-21 20:00:41 +0200

    Added support function for drawing map.

--- kittyTurbo_files/support.c
+++ kittyTurbo_files/support.c
@@ -28,3 +28,41 @@ void __past_icon__(struct context *context,  struct retroScreen *screen, int x, 
 	}
 }
 
+void _draw_map16(struct context *context,struct retroScreen *screen, int px,int py,int mx,int my,int w,int h)
+{
+	int x,y,ppx,ppy;
+	int oy;
+
+	for (y = 0; y<h;y++)
+	{
+		ppy = (y << 4) + py;
+		oy = y + my;
+
+		for (x = 0; x<w;x++)
+		{
+			ppx = ( x << 4) + px;
+
+			__past_icon__( context,  screen,  ppx,  ppy , *((char *) context -> scene_memory + x + mx + (oy * context -> scene_width)  ) );
+		}
+	}
+}
+
+void _draw_map32(struct context *context,struct retroScreen *screen, int px,int py,int mx,int my,int w,int h)
+{
+	int x,y,ppx,ppy;
+	int oy;
+
+	for (y = 0; y<h;y++)
+	{
+		ppy = (y << 5) + py;
+		oy = y + my;
+
+		for (x = 0; x<w;x++)
+		{
+			ppx = ( x << 5) + px;
+
+			__past_icon__( context,  screen,  ppx,  ppy , *((char *) context -> scene_memory + x + mx + (oy * context -> scene_width)  ) );
+		}
+	}
+}
+
--- kittyTurbo_files/support.h
+++ kittyTurbo_files/support.h
@@ -1,3 +1,5 @@
 
-void __past_icon__(struct context *context,  struct retroScreen *screen, int x, int y , int image);
+extern void __past_icon__(struct context *context,  struct retroScreen *screen, int x, int y , int image);
+extern void _draw_map16(struct context *context,struct retroScreen *screen, int px,int py,int mx,int my,int w,int h);
+extern void _draw_map32(struct context *context,struct retroScreen *screen, int px,int py,int mx,int my,int w,int h);
 
